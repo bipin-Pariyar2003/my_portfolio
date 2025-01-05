f305350ee9af9917342a9a4eea2b365e0275ae13
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-01-05 22:11:13 +0545

    about page gariyo

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+#about
+class About(models.Model):
+    short_description = models.TextField()
+    description = models.TextField()
+    about_img = models.ImageField(upload_to = "about")
+
+    class Meta:
+        verbose_name = "About me"
+        verbose_name_plural = "About me"
+
+    def __str__(self):
+        return self.short_description
+    
+
+#Skills
+class Skills(models.Model):
+    skill_name = models.TextField()
+    
+    def __str__(self):
+        return self.skill_name
+    
+
+
+#My Projects
+class My_projects(models.Model):
+    title = models.TextField()
+    project_img = models.ImageField()
+
+    def __str__(self):
+        return self.title
+    
+
+#contact
+class Contact(models.Model):
+    email = models.EmailField()
+    
+
+    def __str__(self):
+        return self.email
--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    projects = My_projects.objects.all()
+    skills = Skills.objects.all()
+    return render(request, 'index.html', {'projects': projects, 'skills': skills})
